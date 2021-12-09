c583176dc029fb0fe0c9231dca766a1a60d32399
PabloCarvajal4 <pablocarvajal128@gmail.com>
2021-12-09 13:33:00 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './pages/menu/menu.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { VotacionComponent } from './pages/votacion/votacion.component';
 
+const routes: Routes = [
+  /* { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }*/
 
-const routes: Routes = [];
+  { path: '', component:MenuComponent,
+    children:[
+      {path:'welcome',component:WelcomeComponent},
+      {path:'votacion',component:VotacionComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
